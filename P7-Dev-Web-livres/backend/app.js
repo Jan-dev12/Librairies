5ebbb944f3094fe8f8473a662e518ca289264e5b
Janek <janekm@hotmail.fr>
2026-04-16 19:17:40 +0200

    connexion et inscription utilisateur valide

--- P7-Dev-Web-livres/backend/app.js
+++ P7-Dev-Web-livres/backend/app.js
@@ -6,12 +6,14 @@ const path = require('path');
 const bookRoutes = require('./routes/book')
 const userRoutes = require('./routes/user')
 
+//connextion a la base de donnée MongoDB
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('Connexion à MongoDB réussie !'))
 .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
 
+//middleware CORS, qui permet au frontend et backend de communiquer sans que le navigateur bloque les requêtes
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -19,9 +21,10 @@ app.use((req, res, next) => {
   next();
 });
 
+//permet à Express de lire le JSON envoyé
 app.use(express.json());
 
-app.use('/api/stuff', bookRoutes);
+app.use('/api/books', bookRoutes);
 app.use('/api/auth', userRoutes);
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
